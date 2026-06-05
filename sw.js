b84c77ce1dd2a3bfb6c01bc6431595239221f9f7
Joe Schmidt <jschmidt@rushtownpoultry.com>
2026-06-05 11:51:50 -0400

    walk log

--- sw.js
+++ sw.js
@@ -47,7 +47,7 @@ self.addEventListener('notificationclick', event => {
 // Returns cached asset INSTANTLY, then refreshes
 // in the background for the next visit.
 // ═══════════════════════════════════════════
-const CACHE_NAME = 'rushtown-v67-trim';
+const CACHE_NAME = 'rushtown-v68-walklogs';
 
 const SHELL_FILES = [
   '/',
